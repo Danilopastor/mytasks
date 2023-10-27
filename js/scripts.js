28d48b945040e3031621738f97f50a4f092e0ba3
Danilopastor <yguinho5@hotmail.com>
2023-10-26 22:35:01 -0300

    feat: implement swipeup and swipedown event and animate

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
+const app = document.getElementById('app')
 const innerApp = document.querySelector(".inner-app")
 const element = Hammer(innerApp)
 
 element.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
 
-element.on("swipe",function(event){
-  console.log(event)
+element.on("swipeup",function(event){
+  app.classList.add("body-full")
+})
+element.on("swipedown",function(event){
+  app.classList.remove("body-full")
 })
